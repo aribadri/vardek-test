ee40c52c1afe41f79c9d9b522b222aadf80d70f2
aribadri <aribad101@icloud.com>
2023-11-03 08:59:35 +0700

    feat: add dependency, vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+
+
+
+export default {
+    root: 'src/',
+    publicDir: '../static/',
+    base: './',
+    server:
+    {
+        host: true,
+    },
+    build:
+    {
+        outDir: '../dist',
+        emptyOutDir: true,
+        sourcemap: true
+    },
+}
