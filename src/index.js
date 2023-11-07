d7892766ec3d877e2b1bb08e2e41effa9cd12eb6
aribadri <aribad101@icloud.com>
2023-11-07 15:15:47 +0700

    feat: add realtime reflection

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 
 // инициализируем переменные
-const minusBtn = document.getElementById('scale-minus');
-const plusBtn = document.getElementById('scale-plus');
+const scaleWidthInput = document.getElementById('scale-width');
+const scaleHeightInput = document.getElementById('scale-height');
 const BACKGROUND_COLOR = new THREE.Color(0xf1f1f1);
 
 
 // cubemap
-const cubeTextureLoader = new THREE.CubeTextureLoader();
-const reflectionCube = cubeTextureLoader.load([
+
+const reflectionCubeTexture = new THREE.CubeTextureLoader().load([
     'textures/posx.jpg', 'textures/negx.jpg',
     'textures/posy.jpg', 'textures/negy.jpg',
     'textures/posz.jpg', 'textures/negz.jpg'
@@ -45,9 +45,9 @@ document.body.appendChild(renderer.domElement);
 const groundGeometry = new THREE.PlaneGeometry(500, 500);
 const groundMaterial = new THREE.MeshPhysicalMaterial({
     color: BACKGROUND_COLOR,
-    roughness: 0.4,
-    metalness: 0.6,
-    envMap: reflectionCube,
+    roughness: 0.2,
+    metalness: 0.8,
+    envMap: reflectionCubeTexture,
 });
 const ground = new THREE.Mesh(groundGeometry, groundMaterial);
 ground.rotation.x = -Math.PI / 2;
@@ -55,47 +55,51 @@ ground.position.y = 0;
 ground.receiveShadow = true;
 scene.add(ground);
 
-// примитив куб
-const cubeGeometry = new THREE.BoxGeometry(2, 2, 2);
-const cubeMaterial = new THREE.MeshPhysicalMaterial({
-    color: 'green',
-    roughness: 0.2,
-    metalness: .4,
-    envMap: reflectionCube,
 
+// reflection
+
+
+const cubeRenderTarget = new THREE.WebGLCubeRenderTarget(512, {
+    generateMipmaps: true,
+    minFilter: THREE.LinearMipmapLinearFilter,
 });
-const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-cube.castShadow = true;
-cube.position.set(-3.5, 1, 5);
-cube.scale.set(0.6, 0.6, 0.6);
-scene.add(cube);
 
+const cubeCamera = new THREE.CubeCamera(0.1, 1000, cubeRenderTarget)
+const pivot = new THREE.Object3D()
+scene.add(pivot)
 
+const reflectionMaterial = new THREE.MeshPhongMaterial({
+    envMap: cubeRenderTarget.texture,
+})
 
 // примитив сфера
-const sphereGeometry = new THREE.SphereGeometry(1, 16, 16);
-const sphereMaterial = new THREE.MeshPhysicalMaterial({
-    color: 'yellow',
-    roughness: 0.2,
-    metalness: .3,
-    envMap: reflectionCube,
+const ball = new THREE.Mesh(new THREE.SphereGeometry(1, 32, 32), reflectionMaterial)
+ball.position.set(3.5, 1, 5);
+ball.castShadow = true
+ball.receiveShadow = true
+ball.add(cubeCamera)
+pivot.add(ball)
 
-});
-const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-sphere.castShadow = true;
-sphere.position.set(3.5, 1, 5);
-sphere.scale.set(0.6, 0.6, 0.6);
-scene.add(sphere);
 
-// устанавливаем материал для двери (меш стекла)
-const doorMaterial = new THREE.MeshStandardMaterial({
+// материал для стекла
+const glassMaterial = new THREE.MeshPhysicalMaterial({
     color: 0xffffff,
     transparent: true,
     opacity: 0.6,
     roughness: 0.1,
     metalness: 0.7,
-    envMap: reflectionCube,
+    envMap: reflectionCubeTexture,
+
 });
+
+// примитив куб
+const cubeGeometry = new THREE.BoxGeometry(2, 2, 2);
+const cube = new THREE.Mesh(cubeGeometry, glassMaterial);
+cube.castShadow = true;
+cube.position.set(-3.5, 1, 5);
+cube.scale.set(0.9, 0.9, 0.9);
+scene.add(cube);
+
 
 // загружаем glb модель
 let door
@@ -106,8 +110,8 @@ modelLoader.load('models/door.glb', (glb) => {
             console.log(child)
             child.castShadow = true;
             child.receiveShadow = true;
-            if (child.name === 'Cube001_Material002_0') {
-                child.material = doorMaterial // устнавливаем отражение на стекло двери
+            if (child.name && child.name === 'Cube001_Material002_0') {
+                child.material = glassMaterial // устнавливаем отражение на стекло двери
             } else {
                 child.material.map = doorTexture // устанавливаем текстуру дерева на дверь
             }
@@ -149,19 +153,22 @@ controls.maxDistance = 35;
 
 
 // уменьшения и увелечения скейла двери
-let currentScale = 0.05;
-let targetScale = currentScale;
-const scaleSpeed = 0.005;
-
-
-const minusScale = () => {
-    targetScale -= scaleSpeed;
-}
-
-const plusScale = () => {
-    targetScale += scaleSpeed;
-
-}
+const onWidthInputChange = () => {
+    const newScaleWidth = parseFloat(scaleWidthInput.value);
+    if (!isNaN(newScaleWidth) && door) {
+      door.scale.x = newScaleWidth;
+      directionLight.shadow.map.needsUpdate = true;
+    }
+  };
+  
+  const onHeightInputChange = () => {
+    const newScaleHeight = parseFloat(scaleHeightInput.value);
+    if (!isNaN(newScaleHeight) && door) {
+      door.scale.y = newScaleHeight;
+      directionLight.shadow.map.needsUpdate = true;
+    }
+  };
+  
 
 // окружение 
 
@@ -176,15 +183,9 @@ scene.add(environmentSphere);
 function animate() {
     requestAnimationFrame(animate);
     controls.update();
-    if (Math.abs(currentScale - targetScale) > 0.001) {
-        currentScale += (targetScale - currentScale) * 0.05;
-        if (door) {
-            door.scale.x = currentScale;
-            door.scale.y = currentScale;
-            door.updateMatrix();
-            directionLight.shadow.map.needsUpdate = true
-        }
-    }
+    onWidthInputChange();
+    onHeightInputChange();
+    cubeCamera.update(renderer, scene)
     renderer.render(scene, camera);
 }
 
@@ -197,8 +198,8 @@ function onWindowResize() {
 }
 
 window.addEventListener('resize', onWindowResize);
-minusBtn.addEventListener('click', minusScale)
-plusBtn.addEventListener('click', plusScale)
+scaleWidthInput.addEventListener('input', onWidthInputChange);
+scaleHeightInput.addEventListener('input', onHeightInputChange);
 
 
 onWindowResize();
